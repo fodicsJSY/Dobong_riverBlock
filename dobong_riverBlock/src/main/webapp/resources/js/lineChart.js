1bc18dea45ae1ff452e368286f2e7fc6582d82ad
정송이 <pso7364@naver.com>
2024-03-19 16:26:47 +0900

    20260319 하천차단+수위/유속/유량

---
+++ dobong_riverBlock/src/main/webapp/resources/js/lineChart.js
@@ -0,0 +1,77 @@
+let daliyCountList;
+let dateDataChart; 
+
+function lineChart(data) {
+    daliyCountList = data.daliyCountList;
+
+    // console.log("daliyCountList", daliyCountList);
+    // console.log("일별 구동 차트");
+
+    // 이전에 있던 차트 객체가 있으면 삭제
+    if (dateDataChart || daliyCountList == null) {
+        dateDataChart.dispose();
+    }
+
+
+
+    dateDataChart = echarts.init(document.getElementById('dateData'));
+
+
+
+    // 카메라 이름 목록 추출
+    const cameras = [...new Set(daliyCountList.map(entry => entry.cameraName))];
+    // console.log("cameras : ", cameras);
+
+    const colors = ['#00A9FF', '#FFB840', '#FF5A46', '#00BD9F', '#785FFF', '#F28B8C', '#989486', '#516F7D', '#28E6EB', '#28695F'];
+    option = {
+        color: colors,
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            orient: 'vertical',
+            right: 0, // 오른쪽으로 이동
+            top: 'middle', // 세로 중앙 정렬
+            textStyle: {
+                color: '#FFFFF' 
+            },
+            data: cameras
+        },
+        grid: {
+            left: '3%',
+            right: '10%',
+            bottom: '5%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category', // x축 타입을 category로 변경
+            boundaryGap: false,
+            data: Array.from({ length: 31 }, (_, i) => i + 1) // 1부터 31까지의 배열 생성
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: cameras.map(camera => {
+            const cameraData = Array.from({ length: 31 }, (_, i) => {
+                const currentDate = i + 1;
+                const matchingEntry = daliyCountList.find(entry => {
+                    const entryDate = new Date(entry.logDate).getDate();
+                    return entryDate === currentDate && entry.cameraName === camera;
+                });
+                return matchingEntry ? parseInt(matchingEntry.cnt) : 0;
+            });
+            // console.log("cameraData : ", cameraData);
+            return {
+                name: camera,
+                type: 'line',
+                data: cameraData
+            };
+        })
+    };
+
+    dateDataChart.setOption(option);
+}
+
+
+
+
---
+++ dobong_riverBlock/src/main/webapp/resources/js/waterLevelChart.js
@@ -0,0 +1,20 @@
+// 수위 bar chart
+
+option = {
+        xAxis: {
+            type: 'category',
+            data: ['지역명1', '지역명2', '지역명3', '지역명4', '지역명5', '지역명6', '지역명7', '지역명8', '지역명9', '지역명10']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [120, 200, 150, 80, 70, 110, 130, 180, 130, 30],
+                type: 'bar',
+                itemStyle: {
+                    color: '#00A9FF' 
+                }
+            }
+        ]
+    };
