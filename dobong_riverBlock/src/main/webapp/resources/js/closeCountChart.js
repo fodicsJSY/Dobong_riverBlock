020b028519f47c71f18b1a55fe49e6cf1852e3f5
정송이 <pso7364@naver.com>
2024-03-22 15:27:23 +0900

    HD, 4k해상도에서 최소높이 고정

--- dobong_riverBlock/src/main/webapp/resources/js/closeCountChart.js
+++ dobong_riverBlock/src/main/webapp/resources/js/closeCountChart.js
@@ -34,38 +34,38 @@ function closeDounutChart(data){
         },
         series: [
             {
-            name: 'Access From',
-            type: 'pie',
-            radius: ['30%', '80%'],
-            avoidLabelOverlap: false,
-            itemStyle: {
-                borderColor: '#1E1E1E',
-                borderWidth: 2
-            },
-            label: {
-                show: true,
-                position: 'inside', // 레이블 위치 설정
-                formatter: '{c}',
-                fontSize: 20, 
-            },
-            emphasis: {
+                name: 'Access From',
+                type: 'pie',
+                radius: ['30%', '80%'],
+                avoidLabelOverlap: false,
+                itemStyle: {
+                    borderColor: '#1E1E1E',
+                    borderWidth: 2
+                },
                 label: {
-                show: false,
-                fontSize: 40,
-                fontWeight: 'bold'
-                }
-            },
-            labelLine: {
-                show: false
-            },
-            data: [
-                { value: closeTimeCnt0, name: '새벽/오전', itemStyle: { color: '#00A9FF' } },
-                { value: closeTimeCnt1, name: '점심시간', itemStyle: { color: '#FFB840' } },
-                { value: closeTimeCnt2, name: '오후', itemStyle: { color: '#FF5A46' } },
-                { value: closeTimeCnt3, name: '퇴근시간', itemStyle: { color: '#00BD9F' } },
-                { value: closeTimeCnt4, name: '저녁', itemStyle: { color: '#785FFF' } },
-                { value: closeTimeCnt5, name: '심야', itemStyle: { color: '#F28B8C' } }
-            ]
+                    show: true,
+                    position: 'inside', // 레이블 위치 설정
+                    formatter: '{c}',
+                    fontSize: 20, 
+                },
+                emphasis: {
+                    label: {
+                    show: false,
+                    fontSize: 40,
+                    fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    { value: closeTimeCnt0, name: '새벽/오전', itemStyle: { color: '#00A9FF' } },
+                    { value: closeTimeCnt1, name: '점심시간', itemStyle: { color: '#FFB840' } },
+                    { value: closeTimeCnt2, name: '오후', itemStyle: { color: '#FF5A46' } },
+                    { value: closeTimeCnt3, name: '퇴근시간', itemStyle: { color: '#00BD9F' } },
+                    { value: closeTimeCnt4, name: '저녁', itemStyle: { color: '#785FFF' } },
+                    { value: closeTimeCnt5, name: '심야', itemStyle: { color: '#F28B8C' } }
+                ]
             }
         ]
     };
